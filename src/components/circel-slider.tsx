11bdd4152bd0b8f2b3542c3553985b4c9b990276
Tinh Ngo <tinhngo@Tinhs-Mac-mini.local>
2018-07-13 08:19:57 +0700

    move SwitchComponent to src folder

--- src/components/circel-slider.tsx
+++ src/components/circel-slider.tsx
@@ -23,7 +23,7 @@ const defaultProps = {
 export interface CircularSliderState {
 }
 
-export default class CircularSliderComponent extends React.Component<CircularSliderProps, CircularSliderState> {
+export default class CircularSliderComponent extends React.PureComponent<CircularSliderProps, CircularSliderState> {
   public static defaultProps = defaultProps;
 
   constructor(props: CircularSliderProps) {
@@ -32,14 +32,15 @@ export default class CircularSliderComponent extends React.Component<CircularSli
 
   render() {
 
-    let width = (this.props.dialRadius * this.props.btnRadius) * 2;
+    // let width = (this.props.dialRadius * this.props.btnRadius) * 2;
+    let width = 500;
     let DR = this.props.dialRadius;
 
     return (
       <View style={styles.container}>
          <Text>CircularSlider Component</Text>
           <Svg width={width} height={width}>
-            {/* <Circle cx={width / 2} cy={width / 2} r={DR} stroke="green" strokeWidth="5" fill="yellow"></Circle> */}
+            <Circle cx={width / 2} cy={width / 2} r={DR} stroke="green" strokeWidth="5" fill="yellow"></Circle>
             <Path stroke="red" strokeWidth="5" fill="none"
               d="M100,50 C140,150  130,150, 250,60"
             ></Path>
